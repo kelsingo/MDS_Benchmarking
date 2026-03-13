b74fe1b1ee56728ffa749efdda1dc16b16ea6245
khue <ngohoangthuykhue@gmail.com>
2026-03-13 18:36:10 +0700

    export data analysis PDF file

--- MDSI/mds_vb/data_analysis.m
+++ MDSI/mds_vb/data_analysis.m
@@ -1,20 +1,25 @@
 %matObj = load("result/LR_102109.mat", "subj_model_parameters");
 %data = matObj.subj_model_parameters(1,1);
-null_spatial = load("result/null_model/LR_102109_15_L.mat", "subj_model_parameters");
-data_null_spatial = null_spatial.subj_model_parameters(1,1);
-%null_temporal = load("result/null_model/LR_102109.mat", "subj_model_parameters");
-%data_null_temporal = null_temporal.subj_model_parameters(1,1);
+pdf_file = "Theta_normal_all_subjects.pdf";
+folder = "result/20260313_result";
+files = dir(fullfile(folder,"*.mat"));
 
-figure('Name', 'Theta Normal Connection Matrices', 'Color', 'w');
-
-for index = 1:8
-    subplot(4, 2, index);
-    non_null = data_null_spatial.Theta_normal(:, :, index); 
-    imagesc(non_null);
-    axis square;
-    colorbar;
-    %if index == 1
-    %    title(["Condition ", num2str(index) "(REAL)"]);
-    %end
+for f = 1:length(files)
+    filepath = fullfile(folder, files(f).name);
+    matobj = load(filepath, "subj_model_parameters");
+    data = matobj.subj_model_parameters(1,1);
+    figure('Name', files(f).name, 'Color', 'W')
     
+    for index = 1:8
+        subplot(4, 2, index);
+        connectivity = data.Theta_normal(:, :, index); 
+        imagesc(connectivity);
+        axis square;
+        colorbar;  
+    end 
+    sgtitle(files(f).name,'Interpreter','none')
+    exportgraphics(gcf, pdf_file, 'Append', true);
+    close(gcf)
 end 
+
+
